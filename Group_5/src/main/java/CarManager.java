9f5fddb1c2be319c7e718afbda8f1ceea7ca3fa1
minh811201 <minhbinhbui811@gmail.com>
2024-10-23 22:02:53 +0700

    CarManager.java

--- Group_5/src/main/java/CarManager.java
+++ Group_5/src/main/java/CarManager.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CarManager {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<String> options = new ArrayList<>();
         options.add("List all brands");
